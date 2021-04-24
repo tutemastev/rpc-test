34afc13415403520d5181fc63f0f37ddd2a5ee67
tutemastev <sbmup593oadg47771@126.com>
2021-04-24 17:28:37 +0800

    repair

--- src/main/java/rpc/client/ClientServiceHandle.java
+++ src/main/java/rpc/client/ClientServiceHandle.java
@@ -5,30 +5,30 @@ import rpc.common.ResponseBeanDTO;
 
 public class ClientServiceHandle {
 
-    
-	public static Object runRpcSericeMethod(RequestBeanDTO request) {
-		Object data = null;
-		ResponseBeanDTO responseBean;
-		try {
-			responseBean = ClientServiceHandle.rpcSendRequest(request);
-			data = responseBean.getData();
-			return data;
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException(e.getMessage());
-		}
-	}
-	
-	private static ResponseBeanDTO rpcSendRequest(RequestBeanDTO request) throws Exception{
-		ResponseBeanDTO result = (ResponseBeanDTO) ClientSocket.oneRpcSericeMethodRun(request);
-		System.out.println("--------client method result :"+result.getData()+"-----------");
-		Exception rpcException = result.getException();
-		if(rpcException != null){
-			rpcException.printStackTrace();
-			throw rpcException;
-		}
-		return result;
-	}
-	
-    
+    public static Object runRpcSericeMethod(RequestBeanDTO request) {
+        Object data = null;
+        ResponseBeanDTO responseBean;
+        try {
+            responseBean = ClientServiceHandle.rpcSendRequest(request);
+            data = responseBean.getData();
+            return data;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private static ResponseBeanDTO rpcSendRequest(RequestBeanDTO request)
+            throws Exception {
+        ResponseBeanDTO result = (ResponseBeanDTO) ClientSocket
+                .oneRpcSericeMethodRun(request);
+        System.out.println("--------client method result :" + result.getData()
+                + "-----------");
+        Exception rpcException = result.getException();
+        if (rpcException != null) {
+            rpcException.printStackTrace();
+            throw rpcException;
+        }
+        return result;
+    }
 }
